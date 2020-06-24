66f56450d711eef6dc2d0bc514459ac36d46eab9
ronniekram <arynn.boniface@gmail.com>
2020-06-24 16:54:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let coolJs = document.querySelector("p#text");
+  coolJs.innerHTML = "This is really cool!"
+});
